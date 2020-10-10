82f75db11c4b9f2c84b55e3baee6ed8bf6c1430b
Ryan Brandt <ryanbrandt@Ryans-MacBook-Pro-8.local>
2020-10-10 18:43:31 -0400

    add resume2

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -16,10 +16,15 @@ function Resume() {
         <>    
    
         <div className='pdf-container'>
+            <iframe src="https://docs.google.com/viewer?srcid=12nJYXBP2xZz3HEgv3sAKfLmrcWvRzeTQ&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="650px" height="1000px"></iframe>
             
-            <embed src="RyanBrandtResume.pdf" width="800px" height="1200px" />
+            
+      
+
+            
            { /*<Document
                 file={"RyanBrandtResume.pdf"}
+         <iframe src="https://drive.google.com/file/d/12nJYXBP2xZz3HEgv3sAKfLmrcWvRzeTQ/view?usp=sharing" frameborder="0"></iframe>
                 <img className='resume-img' src='Resume.png' alt="Resume"></img>
                 >
                 <Page pageNumber={1} width={600} />
